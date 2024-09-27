92701f7150ebf10fb6dd036078d2b91774a1b9d1
gregr <greg.weiqi@gmail.com>
2024-09-27 19:05:45 -0400

    Implement atoms, environments, lookup

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,8 +1,15 @@
 #lang racket
 
+(define (atom?  x)   (or (null? x) (boolean? x) (number? x) (symbol? x)))
+(define (atom?! x)   (unless (atom? x) (error "not an atom" x)))
+(define (atom=? a b) (atom?! a) (atom?! b) (eqv? a b))
+
+(define env.empty '())
+(define (env-extend env k v) (cons (cons k v) env))
 
 (define (lookup env key)
-  TODO)
+  (let ((kv (assq key env)))
+    (if kv (cdr kv) (error "unbound variable" key))))
 
 (define (eval expr env)
   (if (symbol? expr)
