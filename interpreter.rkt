e03d6c37c303bc75b0699658e28c856d379e94e7
Lisa Zhang <lczhang@cs.toronto.edu>
2024-09-27 19:07:52 -0400

    Start quote and cons cases

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,5 +1,8 @@
 #lang racket
 
+(module+ test
+  (require rackunit))
+
 (define (atom?  x)   (or (null? x) (boolean? x) (number? x) (symbol? x)))
 (define (atom?! x)   (unless (atom? x) (error "not an atom" x)))
 (define (atom=? a b) (atom?! a) (atom?! b) (eqv? a b))
@@ -7,13 +10,28 @@
 (define env.empty '())
 (define (env-extend env k v) (cons (cons k v) env))
 
+
 (define (lookup env key)
   (let ((kv (assq key env)))
     (if kv (cdr kv) (error "unbound variable" key))))
 
 (define (eval expr env)
-  (if (symbol? expr)
-      (lookup env expr)
-      '()))
+  (cond
+    [(symbol? expr)
+     (lookup env expr)]
+    [(and (pair? expr) (atom=? (car expr) 'quote))
+     (if (atom=? (cdr (cdr expr) '()))
+         (car (cdr expr))
+         'error)] ; todo
+    [(and (pair? expr) (atom=? (car expr) 'cons))
+     (if (atom=? (cdr (cdr (cdr expr))) '())
+         (cons (car expr) (car (cdr expr))))]
+    [else
+     expr]))
+     
 
+(module+ test
+  (test-equal? "eval atom"
+               (eval '(quote ()))
+               '()))
 
